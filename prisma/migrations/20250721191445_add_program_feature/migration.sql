0c8281cd44eb56754113a43f3866f1a019dd87e2
manavjain2002 <manavj4254@gmail.com>
2025-07-22 00:45:09 +0530

    Fixed migrations

--- prisma/migrations/20250721191445_add_program_feature/migration.sql
+++ prisma/migrations/20250721191445_add_program_feature/migration.sql
@@ -5,13 +5,13 @@ CREATE TABLE "program" (
     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "program_name" TEXT NOT NULL,
     "file_url" TEXT NOT NULL,
+    "program_description" TEXT NOT NULL,
     "program_starting_date" TIMESTAMP(3) NOT NULL,
     "program_ending_date" TIMESTAMP(3) NOT NULL,
-    "program_description" TEXT NOT NULL,
-    "program_location" TEXT NOT NULL,
-    "is_active" BOOLEAN NOT NULL,
     "registration_starting_date" TIMESTAMP(3) NOT NULL,
     "registration_closing_date" TIMESTAMP(3) NOT NULL,
+    "is_program_active" BOOLEAN NOT NULL DEFAULT true,
+    "program_location" TEXT NOT NULL,
     "created_by" INTEGER NOT NULL,
     "organization_id" INTEGER NOT NULL,
 
@@ -21,29 +21,23 @@ CREATE TABLE "program" (
 -- CreateTable
 CREATE TABLE "student_apply_program" (
     "student_apply_program_id" SERIAL NOT NULL,
+    "reg_id" TEXT NOT NULL,
+    "student_id" INTEGER NOT NULL,
+    "program_id" INTEGER NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "reg_id" TEXT NOT NULL,
-    "program_id" INTEGER NOT NULL,
-    "student_id" INTEGER NOT NULL,
 
     CONSTRAINT "student_apply_program_pkey" PRIMARY KEY ("student_apply_program_id")
 );
 
 -- CreateIndex
-CREATE INDEX "program_program_name_program_starting_date_idx" ON "program"("program_name", "program_starting_date");
+CREATE INDEX "program_program_name_idx" ON "program"("program_name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "student_apply_program_reg_id_key" ON "student_apply_program"("reg_id");
 
 -- CreateIndex
-CREATE INDEX "student_apply_program_student_id_program_id_idx" ON "student_apply_program"("student_id", "program_id");
-
--- CreateIndex
 CREATE INDEX "student_apply_program_student_id_idx" ON "student_apply_program"("student_id");
-
--- CreateIndex
-CREATE INDEX "student_apply_program_program_id_idx" ON "student_apply_program"("program_id");
 
 -- AddForeignKey
 ALTER TABLE "program" ADD CONSTRAINT "program_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "teacher"("teacher_id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -52,7 +46,7 @@ ALTER TABLE "program" ADD CONSTRAINT "program_created_by_fkey" FOREIGN KEY ("cre
 ALTER TABLE "program" ADD CONSTRAINT "program_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "organization"("organization_id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "student_apply_program" ADD CONSTRAINT "student_apply_program_program_id_fkey" FOREIGN KEY ("program_id") REFERENCES "program"("program_id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "student_apply_program" ADD CONSTRAINT "student_apply_program_student_id_fkey" FOREIGN KEY ("student_id") REFERENCES "student"("student_id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "student_apply_program" ADD CONSTRAINT "student_apply_program_student_id_fkey" FOREIGN KEY ("student_id") REFERENCES "student"("student_id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "student_apply_program" ADD CONSTRAINT "student_apply_program_program_id_fkey" FOREIGN KEY ("program_id") REFERENCES "program"("program_id") ON DELETE CASCADE ON UPDATE CASCADE;
